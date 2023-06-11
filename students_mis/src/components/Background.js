e021e3282b9175e98415bf122f9c56f75c422112
Serkhani <deviper03@gmail.com>
2023-06-11 23:44:32 +0000

    background component complete

--- students_mis/src/components/Background.js
+++ students_mis/src/components/Background.js
@@ -1,24 +1,17 @@
 import React from 'react';
 // import './App.css'; // Import the CSS file for styling
+import backgroundImage from '../images/engineering bg2.jpg';
+import './Background.css';
 
-import backgroundImage from '../../public/engineering bg2.jpg'; // Import the background image
-
-const App = () => {
-  return (
+const Background = () => {
+    return (
     <div className="container">
-      {/* Apply the background image and set the positioning */}
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        {/* Add the black rectangle with opacity */}
-        <div className="overlay"></div>
-        
-        {/* Rest of your application content */}
-        {/* ... */}
-      </div>
+            
+            <img src={backgroundImage} className='background-image' alt="" />
+            <div className="overlay" />
+                
     </div>
   );
 };
 
-export default App;
+export default Background;
