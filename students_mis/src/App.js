7f54499bc071f9c22a2dc3c008706a28b20e8b0f
Serkhani <deviper03@gmail.com>
2023-06-12 00:32:50 +0000

    added panel files

--- students_mis/src/App.js
+++ students_mis/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Background from './components/background/Background'
+import Panel from './components/panel/Panel';
 
 function App() {
   return (
+    <div>
       <Background />
+      <Panel />
+    </div>
+   
    );
 }
 
---
+++ students_mis/src/components/panel/Panel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Panel.css';
+
+const Panel = () => {
+  return (
+      <div className="panel">
+          sds
+    </div>
+  );
+};
+
+export default Panel;
