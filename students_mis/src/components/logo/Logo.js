62437af40a6be89483a9b009e9efb9a16baeedc3
Serkhani <deviper03@gmail.com>
2023-06-12 02:24:57 +0000

    added logo

---
+++ students_mis/src/components/logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../images/ses_logo.png";
+import './logo.css'
+
+const SES_LOGO = () => {
+  return (
+    <div className="ses_logo">
+      <img src={logo} alt="logo" className="ses_logo" />
+      <span className="ses"> SCHOOL OF ENGINEERING SCIENCES</span>
+    </div>
+  );
+};
+
+export default SES_LOGO;
