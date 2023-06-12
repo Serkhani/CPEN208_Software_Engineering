d0f420da09653f2adca0a2f1de8c7b25932f26a2
Serkhani <deviper03@gmail.com>
2023-06-12 11:12:38 +0000

    added components

--- students_mis/src/components/logo/Logo.js
+++ students_mis/src/components/logo/Logo.js
@@ -5,7 +5,7 @@ import './logo.css'
 const SES_LOGO = () => {
   return (
     <div className="ses_logo">
-      <img src={logo} alt="logo" className="ses_logo" />
+      <img src={logo} alt="logo"/>
       <span className="ses"> SCHOOL OF ENGINEERING SCIENCES</span>
     </div>
   );
--- students_mis/src/components/panel/Panel.js
+++ students_mis/src/components/panel/Panel.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import './Panel.css';
+// import React from "react";
+// import "./Panel.css";
+// import TextField from "../textfield/TextField";
+// import Button from "../button/Button";
+// import SES_LOGO from "../logo/Logo";
+
+// const Panel = () => {
+//   return (
+//     <div className="panel">
+//       <SES_LOGO />
+//       <p className="greetings">Hello! Register to get started</p>
+//       <TextField label={"Name"} />
+//       <TextField label={"Student Id ie: 109*****"} />
+//       <TextField label={"Pin ie: 12345"} />
+//       <TextField label={"Confirm Pin ie: 12345"} />
+//       <p>
+//         Already have an account? <a href="login.html">Login Here</a>
+//       </p>
+//       <Button text={"REGISTER"}/>
+//     </div>
+//   );
+// };
+
+// export default Panel;
+
+
+import React from "react";
+import "./Panel.css";
+import TextField from "../textfield/TextField";
+import Button from "../button/Button";
+import SES_LOGO from "../logo/Logo";
 
 const Panel = () => {
   return (
-      <div className="panel">
-          sds
+    <div className="panel">
+      <SES_LOGO />
+      <p className="greetings">Hello! Register to get started</p>
+      <TextField label={"Name"} />
+      <TextField label={"Student Id ie: 109*****"} />
+      <TextField label={"Pin ie: 12345"} />
+      <TextField label={"Confirm Pin ie: 12345"} />
+      <Button text={"REGISTER"} />
+      <p>
+        Already have an account? <a href="login.html">Login Here</a>
+      </p>
     </div>
   );
 };
