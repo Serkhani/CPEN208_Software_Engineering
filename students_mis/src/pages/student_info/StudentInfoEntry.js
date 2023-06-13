f46d750a8624d8d5e5cc009647afc6ae76185abb
Serkhani <deviper03@gmail.com>
2023-06-13 03:43:09 +0000

    added Student entry page

---
+++ students_mis/src/pages/student_info/StudentInfoEntry.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SES_LOGO from "../../components/logo/Logo";
+import TextField from "../../components/textfield/TextField";
+import Panel from "../../components/panel/Panel";
+
+const StudentInfoEntry = () => {
+  return (
+    <Panel>
+      <SES_LOGO />
+      <TextField />
+    </Panel>
+  );
+};
+
+export default StudentInfoEntry;
